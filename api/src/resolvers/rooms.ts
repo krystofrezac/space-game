c3a21d4a5f1cf38acc8a61612f9b77e0832b7848
Kryštof Řezáč <krystofrezac@gmail.com>
2021-01-20 17:30:42 +0100

    Delete old resolver

--- api/src/resolvers/rooms.ts
+++
@@ -1,9 +0,0 @@
-import { RoomsCallback } from '@space-game/shared/resolvers/rooms';
-
-import { getRooms } from 'stores/room/index';
-
-const rooms = (callback: RoomsCallback): void => {
-  callback(getRooms());
-};
-
-export default rooms;
